9cb38b47f8b6e6ac2bd367fabd993eb0c49c807f
Alvarosintes <asintes20727@iesjoanramis.org>
2023-11-07 13:55:37 +0100

    Tascas11.py

--- Tasca11.py
+++ Tasca11.py
@@ -1,16 +1,91 @@
 def menu_principal():
     print("""
         Menú principal:
-          1. Calculadora enters
-          2. Calculadora reals
+          1. Calculadora de números enters
+          2. Calculadora de números reals
           3. Sortir
           """)
-    x = int(input("Elegeixi una opcio"))
-    if x>0 and x<4:
-        return x
-    else:
-        return 0
-    
-    #programa principal
+    a = int(input("Elegeixi una opcio: "))
+    return a
+def calculadora_enters():
+    op = 1
+    while op>0:
+        print("""
+              Menú enters
+              1. Sumar
+              2. Restar
+              3. Multiplicar
+              4. Dividir
+              5. Sortir
+        """)
+        op = int(input("Elixeixi una opció: "))
+        match op:
+            case 1: #Sumar
+                x = int(input("Introdueixi el primer número: "))
+                y = int(input("Introdueixi el segon número: "))
+                print("{} + {} = {}".format(x, y, x+y))
+            case 2: #Restar
+                x = int(input("Introdueixi el primer número: "))
+                y = int(input("Introdueixi el segon número: "))
+                print("{} - {} = {}".format(x, y, x-y))
+            case 3: #Multiplicar
+                x = int(input("Introdueixi el primer número: "))
+                y = int(input("Introdueixi el segon número: "))
+                print("{} * {} = {}".format(x, y, x*y))
+            case 4: #Dividir
+                x = int(input("Introdueixi el primer número: "))
+                y = int(input("Introdueixi el segon número: "))
+                print("{} / {} = {}".format(x, y, x/y))
+            case 5: #Sortir
+                print("Adéu, ja tornaras a la calculadora inicial \n\n")
+                op=-1
+
+
+def calculadora_reals():
+    op = 1
+    while op>0:
+        print("""
+              Menú reals
+              1. Sumar
+              2. Restar
+              3. Multiplicar
+              4. Dividir
+              5. Sortir
+        """)
+        op = float(input("Elixeixi una opció: "))
+        match op:
+            case 1: #Sumar
+                x = float(input("Introdueixi el primer número: "))
+                y = float(input("Introdueixi el segon número: "))
+                print("{} + {} = {}".format(x, y, x+y))
+            case 2: #Restar
+                x = float(input("Introdueixi el primer número: "))
+                y = float(input("Introdueixi el segon número: "))
+                print("{} - {} = {}".format(x, y, x-y))
+            case 3: #Multiplicar
+                x = float(input("Introdueixi el primer número: "))
+                y = float(input("Introdueixi el segon número: "))
+                print("{} * {} = {}".format(x, y, x*y))
+            case 4: #Dividir
+                x = float(input("Introdueixi el primer número: "))
+                y = float(input("Introdueixi el segon número: "))
+                print("{} / {} = {}".format(x, y, x/y))
+            case 5: #Sortir
+                print("Adéu, ja tornaras a la calculadora inicial \n\n")
+                op=-1
+
+#programa principal
+a = 1
+while a>0 :
     a = menu_principal()
-    print(a)
+    match a :
+        case 1:
+            calculadora_enters()
+        case 2: 
+            calculadora_reals()
+        case 3:
+            a = -1
+        case other:
+            print("Opció no vàlida ") 
+
+    
