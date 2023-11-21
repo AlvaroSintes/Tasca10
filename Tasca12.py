bf523b6e9a9fe8ee23ea17e1b5de0c19c929bd89
Alvarosintes <asintes20727@iesjoanramis.org>
2023-11-21 13:31:11 +0100

    sube

--- Tasca12.py
+++ Tasca12.py
@@ -122,6 +122,42 @@ def octtohex(numero):
     # Post: Escriu en numero en hexadecimal o millor dit, el retorna
     a=int(numero,8)
     return dectohex(a)
+#Hexadecimal a binari, octal i decimal
+def hextonum(hex): # Aquesta funció passa qualsevol hexadecimal a un numero
+    pnum = {
+        "f": 15,
+        "e": 14,
+        "d": 13,
+        "c": 12,
+        "b": 11,
+        "a": 10,
+    }
+    if hex in pnum:
+        return pnum[hex]
+    else:
+        return int(hex)
+def hextodec2(hex):
+    hex = hex.lower()
+    hex = hex[::-1]
+    decimal = 0
+    posicio = 0
+    for digit in hex:
+        valor = hextonum(digit)
+        elevat = 16 ** posicio
+        pnum = elevat * valor
+        decimal += pnum
+        posicio += 1
+    return decimal
+def hextobin(numero):
+    a=int(numero,16)
+    return dectobin(a)
+def hextooct(numero):
+    a=int(numero,16)
+    return dectooct(a)
+def hextodec(numero):
+    a = int(hextodec2(numero))
+    return a
+
 
 def canvis_base():
     op = 1
