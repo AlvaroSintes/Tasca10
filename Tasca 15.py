72a20b69d00db4da81e465b2c624fbb0b050bd1e
Alvarosintes <asintes20727@iesjoanramis.org>
2023-11-24 08:52:02 +0100

    a

---
+++ Tasca 15.py
@@ -0,0 +1,7 @@
+def calcular_longitud(dades):
+    longitud = len(dades)
+    return longitud
+
+a = input("Introdueixi una llista o una cadena de caracters: ")
+longitud_llista = calcular_longitud(a)
+print("Longitud de la llista:", longitud_llista)
